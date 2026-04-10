Fix rotation

validation_tests/simulation3.py
@@ -47,13 +47,13 @@ phantom.user_info.translation = [[0, 0, 0]]
 
 # --- Configuration SPECT ---
 spect, colli, crystal = spect_ge_nm670.add_spect_head(sim, "spect", "megp")
-rad = 60 * cm
+rad = 40 * cm
 pos_x = rad * np.sin(np.radians(current_angle))
 pos_z = rad * np.cos(np.radians(current_angle))
 
 spect.user_info.translation = [[pos_x, 0, pos_z]]
-# rot_matrix = R.from_euler('y', 180 + current_angle, degrees=True).as_matrix()
-# spect.user_info.rotation = [rot_matrix]
+rot_matrix = R.from_euler('y', 180 + current_angle, degrees=True).as_matrix()
+spect.user_info.rotation = [rot_matrix]
 
 # --- Digitizer (Hits & Energy Windows) ---
 F = GateFilterBuilder()
@@ -138,7 +138,7 @@ proj_scat.size = [128, 128]
 proj_scat.output_filename = f"proj_scatter_angle_{int(current_angle)}.mhd"
 
 # --- Sources ---
-total_activity_37mm = 0.1 * MBq / sim.number_of_threads
+total_activity_37mm = 0.01 * MBq / sim.number_of_threads
 radius_ref = 18.5 * mm
 vol_ref = (4/3) * np.pi * (radius_ref**3)
 concentration = total_activity_37mm / vol_ref